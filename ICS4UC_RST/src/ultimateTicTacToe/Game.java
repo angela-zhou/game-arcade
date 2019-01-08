d16eb440394dad65ad1f9c97c8874801c2b1368d
Mstoltz17 <36646065+Mstoltz17@users.noreply.github.com>
2019-01-08 08:58:51 -0500

    Added some files for tic tac toe game.

---
+++ ICS4UC_RST/src/ultimateTicTacToe/Game.java
@@ -0,0 +1,16 @@
+package ultimateTicTacToe;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application{
+
+	@Override
+	public void start(Stage stgMain) throws Exception {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
