ebf6ca196b44f3646cfa4afbde692b119ed01291
Mstoltz17 <36646065+Mstoltz17@users.noreply.github.com>
2019-01-16 09:17:58 -0500

    Testing.

--- ICS4UC_RST/src/pong/PongServer.java
+++ ICS4UC_RST/src/pong/PongServer.java
@@ -8,18 +8,21 @@ import java.util.ArrayList;
 
 public class PongServer {
 
+	//https://www.pegaxchange.com/2017/12/07/simple-tcp-ip-server-client-java/
+	
 	static ArrayList<Socket> clients = new ArrayList<Socket>();
 	static ArrayList<PrintWriter> writers = new ArrayList<PrintWriter>();
 		   
 	public static void main(String[] args) throws IOException{
 //		int portNumber = Integer.parseInt(args[0]);
-		try(ServerSocket serverSocket = new ServerSocket(9090)) {
-			System.out.println("Server Started!");
+		try(ServerSocket serverSocket = new ServerSocket(23)) {
+			System.out.println("Server Started!\n" + serverSocket.getInetAddress().getHostAddress());
 			while(true) {
 				try(Socket client = serverSocket.accept()) {
-					clients.add(client);
+					System.out.println("User Connected");
+//					clients.add(client);
 					PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-					writers.add(out);
+//					writers.add(out);
 					out.println("Hello");
 				}
 			}
