540a4aee05940ae77158bcfe18c57d06fbce387c
Mstoltz17 <mstoltzmc@gmail.com>
2019-01-17 00:24:41 -0500

    Maybe this will work... probably not

--- ICS4UC_RST/src/pong/PongClient.java
+++ ICS4UC_RST/src/pong/PongClient.java
@@ -3,18 +3,38 @@ package pong;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class PongClient {
 
-	public static void main(String[] args) throws IOException{
-        System.out.println("Enter the IP address and then port of a machine running the date server: ");
-        String serverAddress = new Scanner(System.in).nextLine();
-        String port = new Scanner(System.in).nextLine();
-        Socket socket = new Socket(serverAddress, Integer.parseInt(port));
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String response = in.readLine();
-        System.out.println("Server response: " + response);
+	private Socket socket;
+	private Scanner scanner;
+	
+	private PongClient(String serverAddress, int serverPort) throws Exception {
+		this.socket = new Socket(serverAddress, serverPort);
+		this.scanner = new Scanner(System.in);
+	}
+
+	private void start() throws IOException {
+		String input;
+		
+		while(true) {
+			input = scanner.nextLine();
+			
+			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+			out.println(input);
+			out.flush();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		PongClient client = new PongClient("localhost", 52616);
+		
+		System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+		client.start();
 	}
 }
--- ICS4UC_RST/src/pong/PongServer.java
+++ ICS4UC_RST/src/pong/PongServer.java
@@ -1,7 +1,10 @@
 package pong;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -10,23 +13,42 @@ public class PongServer {
 
 	//https://www.pegaxchange.com/2017/12/07/simple-tcp-ip-server-client-java/
 	
-	static ArrayList<Socket> clients = new ArrayList<Socket>();
-	static ArrayList<PrintWriter> writers = new ArrayList<PrintWriter>();
-		   
-	public static void main(String[] args) throws IOException{
-//		int portNumber = Integer.parseInt(args[0]);
-		try(ServerSocket serverSocket = new ServerSocket(23)) {
-			System.out.println("Server Started!\n" + serverSocket.getInetAddress().getHostAddress());
-			while(true) {
-				try(Socket client = serverSocket.accept()) {
-					System.out.println("User Connected");
-//					clients.add(client);
-					PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-//					writers.add(out);
-					out.println("Hello");
-				}
-			}
-		}
-
+	private ServerSocket server;
+	
+	public PongServer(String ipAddress) throws Exception {
+		if(ipAddress != null && !ipAddress.isEmpty())
+			this.server = new ServerSocket(0, 1, InetAddress.getByName(ipAddress));
+		else
+			this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
 	}
+	
+	private void listen() throws Exception {
+		String data = null;
+		Socket client = this.server.accept();
+		String clientAddress = client.getInetAddress().getHostAddress();
+		System.out.println("\r\nNew connection from " + clientAddress);
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		
+		while((data = in.readLine()) != null) {
+			System.out.println("\r\nMessage from " + clientAddress + ": " + data);
+		}
+	}
+		 
+	public InetAddress getSocketAddress() {
+		return this.server.getInetAddress();
+	}
+	
+	public int getPort() {
+		return this.server.getLocalPort();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		PongServer app = new PongServer("localhost");
+		
+		System.out.println("\r\nRunning Server: " + "\nHost: " + app.getSocketAddress().getHostAddress() + "\nPort: " + app.getPort());
+		
+		app.listen();
+	}	
 }
