c10db39bd7cbc0ecd0e6649469c2d29139471574
Mstoltz17 <36646065+Mstoltz17@users.noreply.github.com>
2019-01-17 08:50:52 -0500

    Attemp #3

--- ICS4UC_RST/src/pong/PongClient.java
+++ ICS4UC_RST/src/pong/PongClient.java
@@ -1,40 +1,45 @@
 package pong;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class PongClient {
-
-	private Socket socket;
-	private Scanner scanner;
 	
-	private PongClient(String serverAddress, int serverPort) throws Exception {
-		this.socket = new Socket(serverAddress, serverPort);
-		this.scanner = new Scanner(System.in);
-	}
-
-	private void start() throws IOException {
-		String input;
-		
-		while(true) {
-			input = scanner.nextLine();
-			
-			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-			out.println(input);
-			out.flush();
-		}
-	}
+	public static void main(String[] args) throws IOException {
+	    Socket socket = new Socket("localhost", 12900);
+	    System.out.println("Started client  socket at "
+	        + socket.getLocalSocketAddress());
+	    BufferedReader socketReader = new BufferedReader(new InputStreamReader(
+	        socket.getInputStream()));
+	    BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(
+	        socket.getOutputStream()));
+	    BufferedReader consoleReader = new BufferedReader(
+	        new InputStreamReader(System.in));
 	
-	public static void main(String[] args) throws Exception{
-		PongClient client = new PongClient("localhost", 52616);
-		
-		System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
-		client.start();
+	    String promptMsg = "Please enter a  message  (Bye  to quit):";
+	    String outMsg = null;
+	
+	    System.out.print(promptMsg);
+	    while ((outMsg = consoleReader.readLine()) != null) {
+	      if (outMsg.equalsIgnoreCase("bye")) {
+	        break;
+	      }
+	      // Add a new line to the message to the server,
+	      // because the server reads one line at a time.
+	      socketWriter.write(outMsg);
+	      socketWriter.write("\n");
+	      socketWriter.flush();
+	
+	      // Read and display the message from the server
+	      String inMsg = socketReader.readLine();
+	      System.out.println("Server: " + inMsg);
+	      System.out.println(); // Print a blank line
+	      System.out.print(promptMsg);
+	    }
+	    socket.close();
 	}
 }
--- ICS4UC_RST/src/pong/PongServer.java
+++ ICS4UC_RST/src/pong/PongServer.java
@@ -1,54 +1,52 @@
 package pong;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class PongServer {
+	
+	 public static void handleClientRequest(Socket socket) {
+		    try{
+		      BufferedReader socketReader = null;
+		      BufferedWriter socketWriter = null;
+		      socketReader = new BufferedReader(new InputStreamReader(
+		          socket.getInputStream()));
+		      socketWriter = new BufferedWriter(new OutputStreamWriter(
+		          socket.getOutputStream()));
 
-	//https://www.pegaxchange.com/2017/12/07/simple-tcp-ip-server-client-java/
-	
-	private ServerSocket server;
-	
-	public PongServer(String ipAddress) throws Exception {
-		if(ipAddress != null && !ipAddress.isEmpty())
-			this.server = new ServerSocket(0, 1, InetAddress.getByName(ipAddress));
-		else
-			this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
-	}
-	
-	private void listen() throws Exception {
-		String data = null;
-		Socket client = this.server.accept();
-		String clientAddress = client.getInetAddress().getHostAddress();
-		System.out.println("\r\nNew connection from " + clientAddress);
-		
-		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		
-		while((data = in.readLine()) != null) {
-			System.out.println("\r\nMessage from " + clientAddress + ": " + data);
-		}
-	}
-		 
-	public InetAddress getSocketAddress() {
-		return this.server.getInetAddress();
-	}
-	
-	public int getPort() {
-		return this.server.getLocalPort();
-	}
+		      String inMsg = null;
+		      while ((inMsg = socketReader.readLine()) != null) {
+		        System.out.println("Received from  client: " + inMsg);
+
+		        String outMsg = inMsg;
+		        socketWriter.write(outMsg);
+		        socketWriter.write("\n");
+		        socketWriter.flush();
+		      }
+		      socket.close();
+		    }catch(Exception e){
+		      e.printStackTrace();
+		    }
+	 }
 	
 	public static void main(String[] args) throws Exception {
-		
-		PongServer app = new PongServer("localhost");
-		
-		System.out.println("\r\nRunning Server: " + "\nHost: " + app.getSocketAddress().getHostAddress() + "\nPort: " + app.getPort());
-		
-		app.listen();
+	    ServerSocket serverSocket = new ServerSocket(12900, 100,
+	            InetAddress.getByName("localhost"));
+	    System.out.println("Server started  at:  " + serverSocket);
+	    
+	    while(true) {
+	        System.out.println("Waiting for a  connection...");
+
+	        final Socket activeSocket = serverSocket.accept();
+
+	        System.out.println("Received a  connection from  " + activeSocket);
+	        Runnable runnable = () -> handleClientRequest(activeSocket);
+	        new Thread(runnable).start();
+	    }
 	}	
 }
