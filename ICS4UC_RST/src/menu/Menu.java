0e60b8918fa76a8a091eb7a98a022ed19f2b945a
Angela Zhou <angela.zhou1@ocsbstudent.ca>
2019-01-20 12:11:01 -0500

    Added background graphics & made some visual changes (now looks pretty!)

--- ICS4UC_RST/src/menu/Menu.java
+++ ICS4UC_RST/src/menu/Menu.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 public class Menu extends Application{
 
 	private static Menu instance;
+	// instances of games
 	asteroids.Game astGame = new asteroids.Game();
 	pacMan.PacMan pacGame = new pacMan.PacMan();
 	
@@ -22,10 +23,11 @@ public class Menu extends Application{
 		stgMenu.show();
 	}
 	
+	
+	/**
+	 * Methods to start a new stage with games
+	 */
 	public void runAsteroids() {
-		//Looking to start a game? easy peasy
-		//Look at the top, use the package name and MAIN CLASS (the one with the launch) to make an instance of the game
-		//Call the start method on the instance with a new stage and boom, your game is running
 		try {
 			astGame.start(new Stage());
 		} catch (Exception e) {
@@ -37,7 +39,7 @@ public class Menu extends Application{
 		try {
 			pacGame.start(new Stage());
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Unable to start game ### Error @ Menu.java - public void runPac()");
 		}
 	}
 	
